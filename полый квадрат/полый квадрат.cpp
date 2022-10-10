c9b17f7a9c8dd4b7a560039562e3d4d71fcc5e0a
Ирина <@DESKTOP-MSGGBKL>
2022-10-10 23:10:46 +0500

    Добавьте файлы проекта.

---
+++ полый квадрат/полый квадрат.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "rus");
+    const char star = '*';
+    int size;
+    cout << "Введите размер квадрата : ";
+    cin >> size;
+
+    for (int i = 0; i < size; i++)
+          cout << star;
+    cout << endl;
+
+    for (int i = 0; i < size - 1; i++)
+        cout << star << setw(size - 1) << star << endl;
+
+    for (int i = 0; i < size; i++)
+        cout << star;
+    cout << endl;
+
+    return 0;
+}
