e7a6fc3eafa82233e628dcc363e516424a4515ab
Irissha <110852608+Irissha@users.noreply.github.com>
2022-10-19 23:05:43 +0500

    Update полый квадрат.cpp

--- полый квадрат/полый квадрат.cpp
+++ полый квадрат/полый квадрат.cpp
@@ -12,7 +12,7 @@ int main() {
           cout << star;
     cout << endl;
 
-    for (int i = 0; i < size - 1; i++)
+    for (int i = 0; i < size - 2; i++)
         cout << star << setw(size - 1) << star << endl;
 
     for (int i = 0; i < size; i++)
